d366237de27ddfd43245d70dc6dca5de940e6713
tasosthd <8r8sy7x5ys@privaterelay.appleid.com>
2026-05-19 10:14:18 +0300

    Update feed.js

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -340,7 +340,7 @@ function renderThreads() {
               <img src="${escapeHTML(avatar)}" alt="${escapeHTML(name)} avatar" />
               <div>
                 <strong>${escapeHTML(name)}</strong>
-                <span>${escapeHTML(username)} Â· ${escapeHTML(date)}</span>
+                <span>${escapeHTML(username)} · ${escapeHTML(date)}</span>
               </div>
             </button>
 
